9e8deba0e18839f43babd6216c953ea30b48a790
Berkus Decker <berkus+github@metta.systems>
2019-01-14 21:07:14 +0200

    Use qemu with Peter Maydell's patches for Mojave
    See https://bugs.launchpad.net/qemu/+bug/1802684

--- .cargo/runscript.sh
+++ .cargo/runscript.sh
@@ -2,5 +2,5 @@
 
 aarch64-unknown-linux-musl-objcopy -O binary $1 $1.bin
 
-# -d unimp,int -serial stdio -S
-qemu-system-aarch64 -M raspi3 -d in_asm -serial null -serial stdio -kernel $1.bin
+# -d in_asm,unimp,int -S
+/usr/local/Cellar/qemu/HEAD-3365de01b5-custom/bin/qemu-system-aarch64 -M raspi3 -serial null -serial stdio -kernel $1.bin
