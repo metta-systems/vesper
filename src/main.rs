21303497cd267997064bd491afa86a626a6749df
Berkus Decker <berkus+github@metta.systems>
2019-03-02 19:18:09 +0200

    [wip] drop qemu exit code (which doesn't work anyway)

--- src/main.rs
+++ src/main.rs
@@ -176,28 +176,28 @@ entry!(kmain);
 
 // From https://stackoverflow.com/a/49930361/895245
 // @todo specify exit value depending on tests result?
-fn qemu_aarch64_exit() -> ! {
-    unsafe {
-        asm!("
-            /* 0x20026 == ADP_Stopped_ApplicationExit */
-            mov x1, #0x26
-            movk x1, #2, lsl #16
-            str x1, [sp,#0]
+// fn qemu_aarch64_exit() -> ! {
+//     unsafe {
+//         asm!("
+//             /* 0x20026 == ADP_Stopped_ApplicationExit */
+//             mov x1, #0x26
+//             movk x1, #2, lsl #16
+//             str x1, [sp,#0]
 
-            /* Exit status code. Host QEMU process exits with that status. */
-            mov x0, #0
-            str x0, [sp,#8]
+//             /* Exit status code. Host QEMU process exits with that status. */
+//             mov x0, #0
+//             str x0, [sp,#8]
 
-            /* x1 contains the address of parameter block.
-             * Any memory address could be used. */
-            mov x1, sp
+//             /* x1 contains the address of parameter block.
+//              * Any memory address could be used. */
+//             mov x1, sp
 
-            /* SYS_EXIT */
-            mov w0, #0x18
+//             /* SYS_EXIT */
+//             mov w0, #0x18
 
-            /* Do the semihosting call on A64. */
-            hlt 0xf000"
-        :::: "volatile");
-    }
-    unreachable!();
-}
+//             /* Do the semihosting call on A64. */
+//             hlt 0xf000"
+//         :::: "volatile");
+//     }
+//     unreachable!();
+// }
