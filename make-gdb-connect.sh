703b9fb4db6cdb89f3e990e66b1d5217d3b2d668
Berkus Decker <berkus+github@metta.systems>
2019-03-03 22:10:00 +0200

    sq

--- make-gdb-connect.sh
+++ make-gdb-connect.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # Generate gdb-connect script with given RTT block address, to avoid typing it in manually
 
+# Hint from this answer https://superuser.com/a/747905/355774
 [ $# -ge 1 ] && ADDR="$1" || ADDR=$(cat)
 
 cat <<EOF > gdb-connect
