eaa2698c752fcc4e6e3836815fe074baf5e93309
Berkus Decker <berkus+github@metta.systems>
2020-11-20 03:26:37 +0200

    [wip] Add x86_64 arch

--- nucleus/src/arch/mod.rs
+++ nucleus/src/arch/mod.rs
@@ -2,9 +2,15 @@
  * SPDX-License-Identifier: BlueOak-1.0.0
  * Copyright (c) Berkus Decker <berkus+vesper@metta.systems>
  */
-
-#[cfg(target_arch = "aarch64")]
-#[macro_use]
-pub mod aarch64;
-#[cfg(target_arch = "aarch64")]
-pub use self::aarch64::*;
+cfg_if::cfg_if! {
+    if #[cfg(target_arch = "aarch64")] {
+        #[macro_use]
+        pub mod aarch64;
+        pub use self::aarch64::*;
+    }
+    if #[cfg(target_arch = "x86_64")] {
+        #[macro_use]
+        pub mod x86_64;
+        pub use self::x86_64::*;
+    }
+}
---
+++ nucleus/src/arch/x86_64/mod.rs
@@ -0,0 +1,4 @@
+/*
+ * SPDX-License-Identifier: BlueOak-1.0.0
+ */
+// mod arch::x86_64
