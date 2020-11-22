f15aeff5128d2521f4d6a72b3c9895760a4b3ccd
Berkus Decker <berkus+github@metta.systems>
2020-11-22 15:49:22 +0200

    [blog] Move general comments to separate file

--- machine/src/arch/aarch64/caps.rs
+++ machine/src/arch/aarch64/caps.rs
@@ -438,14 +438,6 @@ impl CapTableEntry {
     }
 }
 
-// The different libraries seem to target different use-cases, though. For example, `snafu` with its
-// strongly typed errors and contexts seems to be a good fit for libraries. On the other hand,
-// anyhow with its focus on the type-erased Error and on creating string errors and contexts seems
-// to be more useful for applications. After all, errors produced by libraries need to be understood
-// by other code, errors produced by executables need to be understood by humans.
-// -- https://lukaskalbertodt.github.io/2019/11/14/thoughts-on-error-handling-in-rust.html
-// @see also https://blog.yoshuawuyts.com/error-handling-survey/
-// @see also https://www.reddit.com/r/rust/comments/dfs1zk/2019_q4_error_patterns_snafu_vs_errderive_anyhow/
 #[derive(Debug)]
 pub enum CapError {
     CannotCreate,
