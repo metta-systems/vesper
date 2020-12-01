9544de9ebdbd50168e7924c1cbd4830b0988b942
Berkus Decker <berkus+github@metta.systems>
2020-12-02 01:06:14 +0200

    [wip] kernel objs

--- machine/src/arch/aarch64/objects.rs
+++ machine/src/arch/aarch64/objects.rs
@@ -263,7 +263,10 @@ trait Thread {
 
 // @todo <<SchedContext>>
 
-struct TCB {}
+struct TCB {
+    capability: u128, // should actually be a CapPath here - this is the argument to
+    // Thread.read_registers(cap, ... call for example.
+}
 
 impl Thread for TCB {}
 impl KernelObject for TCB {
@@ -316,7 +319,7 @@ impl API for Kernel {}
 
 trait DomainSet {
     // ??
-    fn set(domain, thread: TCB);
+    fn set(domain: Dom, thread: TCB);
 }
 
 // Virtualisation
