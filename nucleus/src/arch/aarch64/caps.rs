b6852b46a5a144b2813bdfb7eb38ab073969b35a
Berkus Decker <berkus+github@metta.systems>
2020-12-13 19:42:29 +0200

    wip move capnode cap impl

--- nucleus/src/arch/aarch64/caps.rs
+++ nucleus/src/arch/aarch64/caps.rs
@@ -470,6 +470,19 @@ impl EndpointCapability {}
 // by having a root capnode cap we can traverse the whole tree.
 
 impl CapNodeCapability {
+    /// Create a capability to CapNode.
+    ///
+    /// CapNode capabilities allow to address a capability node tree entry.
+    pub fn new(pptr: u64, radix: u32, guard_size: u32, guard: u64) -> CapNodeCapability {
+        CapNodeCapability(LocalRegisterCopy::new(u128::from(
+            CapNodeCap::Type::value
+                + CapNodeCap::Radix.val(radix.into())
+                + CapNodeCap::GuardSize.val(guard_size.into())
+                + CapNodeCap::Guard.val(guard.into())
+                + CapNodeCap::Ptr.val(pptr.into()),
+        )))
+    }
+
     /// Create new root node.
     pub fn new_root(pptr: u64) -> CapNodeCapability {
         const CONFIG_ROOT_CAPNODE_SIZE_BITS: u32 = 12;
@@ -502,21 +515,6 @@ impl CapNodeCapability {
         slice[slot].derivation = DerivationTreeNode::empty()
             .set_revocable(true)
             .set_first_badged(true);
-    }
-}
-
-impl CapNodeCapability {
-    /// Create a capability to CapNode.
-    ///
-    /// CapNode capabilities allow to address a capability node tree entry.
-    pub fn new(pptr: u64, radix: u32, guard_size: u32, guard: u64) -> CapNodeCapability {
-        CapNodeCapability(LocalRegisterCopy::new(u128::from(
-            CapNodeCap::Type::value
-                + CapNodeCap::Radix.val(radix.into())
-                + CapNodeCap::GuardSize.val(guard_size.into())
-                + CapNodeCap::Guard.val(guard.into())
-                + CapNodeCap::Ptr.val(pptr.into()),
-        )))
     }
 }
 
