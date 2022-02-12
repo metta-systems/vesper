98270c9ebd6b1497e624fb869feb6768239d355b
Berkus Decker <berkus+github@metta.systems>
2022-02-12 02:34:09 +0200

    wip: try to use fdt-rs for DTB parsing

--- nucleus/src/main.rs
+++ nucleus/src/main.rs
@@ -32,6 +32,7 @@ use machine::devices::serial::SerialOps;
 use {
     cfg_if::cfg_if,
     core::{cell::UnsafeCell, time::Duration},
+    fdt_rs::base::DevTree,
     machine::{arch, console::console, entry, exception, info, memory, println, time, warn},
 };
 
@@ -126,12 +127,11 @@ pub fn kernel_main(dtb: u32) -> ! {
     println!("DTB loaded at {:x}", dtb);
 
     // Safety: we got the address from the bootloader, if it lied - well, we're screwed!
-    let device_tree = crate::device_tree::DeadTree::new(unsafe {
-        dtb::Reader::read_from_address(dtb as usize).expect("DeviceTree not found")
-    });
+    let device_tree =
+        unsafe { DevTree::from_raw_pointer(dtb as *const _).expect("DeviceTree failed to read") };
 
     // List unusable memory, and remove it from the memory regions for the allocator.
-    for entry in device_tree.reserved_mem_entries() {
+    for entry in device_tree.compatible_nodes("memory") {
         println!("reserved: {:?} bytes at {:?}", entry.size, entry.address);
     }
     // Also, remove the DTB memory region.
