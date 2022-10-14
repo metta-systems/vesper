5a808c4548a43aba52dcca55bebc80f59433e19f
Berkus Decker <berkus+github@metta.systems>
2022-10-15 02:56:30 +0300

    wip: Drop unused function

--- bin/chainboot/src/stdmem.rs
+++ bin/chainboot/src/stdmem.rs
@@ -1,6 +1,6 @@
 #[inline(always)]
 #[no_mangle]
-#[allow(unused)]
+// #[allow(unused)]
 #[link_section = ".text.stdmem"]
 #[cfg(not(feature = "asm"))]
 pub unsafe extern "C" fn local_memcpy(mut dest: *mut u8, mut src: *const u8, n: usize) {
@@ -12,15 +12,15 @@ pub unsafe extern "C" fn local_memcpy(mut dest: *mut u8, mut src: *const u8, n: 
     }
 }
 
-#[inline(always)]
-#[no_mangle]
-#[allow(unused)]
-#[link_section = ".text.stdmem"]
-#[cfg(not(feature = "asm"))]
-pub unsafe extern "C" fn local_memset(mut s: *mut u8, c: u8, n: usize) {
-    let end = s.add(n);
-    while s < end {
-        *s = c;
-        s = s.add(1);
-    }
-}
+// #[inline(always)]
+// #[no_mangle]
+// #[allow(unused)]
+// #[link_section = ".text.stdmem"]
+// #[cfg(not(feature = "asm"))]
+// pub unsafe extern "C" fn local_memset(mut s: *mut u8, c: u8, n: usize) {
+//     let end = s.add(n);
+//     while s < end {
+//         *s = c;
+//         s = s.add(1);
+//     }
+// }
