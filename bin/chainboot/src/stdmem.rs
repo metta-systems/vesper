2245e4d7c20dfba41b8100f723fe65303f15fda2
Berkus Decker <berkus+github@metta.systems>
2022-10-19 00:47:07 +0300

    chore: Drop unused function

--- bin/chainboot/src/stdmem.rs
+++ bin/chainboot/src/stdmem.rs
@@ -11,16 +11,3 @@ pub unsafe extern "C" fn local_memcpy(mut dest: *mut u8, mut src: *const u8, n: 
         src = src.add(1);
     }
 }
-
-// #[inline(always)]
-// #[no_mangle]
-// #[allow(unused)]
-// #[link_section = ".text.stdmem"]
-// #[cfg(not(feature = "asm"))]
-// pub unsafe extern "C" fn local_memset(mut s: *mut u8, c: u8, n: usize) {
-//     let end = s.add(n);
-//     while s < end {
-//         *s = c;
-//         s = s.add(1);
-//     }
-// }
