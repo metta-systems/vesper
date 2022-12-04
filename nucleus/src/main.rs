cab5df35b169444f01d3595a74c82162bfc98e9f
Berkus Decker <berkus+github@metta.systems>
2022-12-04 15:51:35 +0200

    wip: this runs fine!

--- nucleus/src/main.rs
+++ nucleus/src/main.rs
@@ -17,29 +17,31 @@
 #![reexport_test_harness_main = "test_main"]
 #![allow(missing_docs)]
 #![deny(warnings)]
-#![deny(unused)]
+#![allow(unused)]
 #![feature(allocator_api)]
 
 use armv8a_panic_semihosting as _;
 
-use machine::entry; //, DMA_ALLOCATOR};
-
-entry!(kernel_main);
+machine::entry!(kernel_main);
 
 /// Kernel entry point.
 /// `arch` crate is responsible for calling it.
 // #[inline]
 pub fn kernel_main() -> ! {
-    if armv8a_semihosting::hprintln!("Lets go!").is_err() {
-        // opening semihosting stdout fails!
-        armv8a_semihosting::debug::exit(armv8a_semihosting::debug::EXIT_FAILURE);
-    }
+    armv8a_semihosting::hprintln!("Lets go!").ok(); // {
+                                                    //     // opening semihosting stdout fails!
+                                                    //     armv8a_semihosting::debug::exit(armv8a_semihosting::debug::EXIT_FAILURE);
+                                                    // }
 
-    // use core::alloc::{Allocator, Layout};
+    use {
+        core::alloc::{Allocator, Layout},
+        machine::DMA_ALLOCATOR,
+    };
 
-    // DMA_ALLOCATOR
-    //     .lock(|a| a.allocate(Layout::from_size_align(1024, 16).unwrap()))
-    //     .unwrap();
+    DMA_ALLOCATOR
+        .lock(|a| a.allocate(Layout::from_size_align(1024, 16).unwrap()))
+        .unwrap();
 
+    armv8a_semihosting::hprintln!("Lets go 2!").ok();
     panic!("Off you go!");
 }
