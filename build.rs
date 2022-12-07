e426a5c0ff70eceb3ca001d1a569a57d27df491b
Berkus Decker <berkus+github@metta.systems>
2022-12-07 22:46:54 +0200

    revert: "wip: track linker script"
    410365e4cbeaf4d9c2af7ba09f6033f38dfcfeea

--- build.rs
+++ build.rs
@@ -1,8 +1,6 @@
 const LINKER_SCRIPT: &str = "linker/aarch64.ld";
-const LINKER_SCRIPT_AUX: &str = "linker/aarch64-exceptions.ld";
 
 fn main() {
     println!("cargo:rerun-if-changed={}", LINKER_SCRIPT);
-    println!("cargo:rerun-if-changed={}", LINKER_SCRIPT_AUX);
     println!("cargo:rustc-link-arg=--script={}", LINKER_SCRIPT);
 }
