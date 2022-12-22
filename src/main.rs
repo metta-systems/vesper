7d0cf7ec679ffb4939505853620aa05650b20035
Berkus Decker <berkus+github@metta.systems>
2022-12-22 14:53:26 +0200

    wip: enable rpi4 build again

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,97 @@
 #![no_std]
 #![no_main]
+#![feature(decl_macro)]
+#![feature(format_args_nl)]
+
+use core::{fmt::Write, marker::PhantomData, ops};
 
 #[cfg(not(target_arch = "aarch64"))]
 use architecture_not_supported_sorry;
 
 pub mod boot;
+pub mod gpio;
+pub mod mini_uart;
+
+pub struct BcmHost;
+
+impl BcmHost {
+    /// At which address to load the kernel binary.
+    pub const fn kernel_load_address() -> u64 {
+        0x8_0000
+    }
+
+    /// As per <https://www.raspberrypi.org/forums/viewtopic.php?p=1170522#p1170522>
+    ///
+    pub fn bus2phys(bus: usize) -> usize {
+        bus & !0xc000_0000
+    }
+
+    pub fn phys2bus(phys: usize) -> usize {
+        phys | 0xc000_0000
+    }
+
+    /// Name of the hardware device this BcmHost is compiled for.
+    pub const fn board_name() -> &'static str {
+        "Raspberry Pi 4+"
+    }
+
+    /// This returns the ARM-side physical address where peripherals are mapped.
+    ///
+    pub const fn get_peripheral_address() -> usize {
+        0xfe00_0000
+    }
+
+    /// This returns the size of the peripherals' space.
+    pub const fn get_peripheral_size() -> usize {
+        0x0180_0000
+    }
+
+    /// This returns the bus address of the SDRAM.
+    pub const fn get_sdram_address() -> usize {
+        0xc000_0000 // uncached
+    }
+}
+
+pub struct MMIODerefWrapper<T> {
+    base_addr: usize,
+    phantom: PhantomData<fn() -> T>,
+}
+
+//--------------------------------------------------------------------------------------------------
+// Public Code
+//--------------------------------------------------------------------------------------------------
+
+impl<T> MMIODerefWrapper<T> {
+    /// Create an instance.
+    ///
+    /// # Safety
+    ///
+    /// Unsafe, duh!
+    pub const unsafe fn new(start_addr: usize) -> Self {
+        Self {
+            base_addr: start_addr,
+            phantom: PhantomData,
+        }
+    }
+}
+
+/// Deref to RegisterBlock
+///
+/// Allows writing
+/// ```
+/// self.GPPUD.read()
+/// ```
+/// instead of something along the lines of
+/// ```
+/// unsafe { (*GPIO::ptr()).GPPUD.read() }
+/// ```
+impl<T> ops::Deref for MMIODerefWrapper<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*(self.base_addr as *const _) }
+    }
+}
 
 /// Loop forever in sleep mode.
 #[inline]
@@ -14,8 +101,6 @@ pub fn endless_sleep() -> ! {
     }
 }
 
-use armv8a_panic_semihosting as _;
-
 #[export_name = "main"]
 #[inline(always)]
 pub unsafe fn __main() -> ! {
@@ -24,12 +109,21 @@ pub unsafe fn __main() -> ! {
 
 /// Kernel entry point.
 pub fn kernel_main() -> ! {
-    armv8a_semihosting::hprintln!("Letsgo!").ok();
+    let gpio = gpio::GPIO::default();
+    let mut uart = mini_uart::MiniUart::default().prepare(&gpio);
+
+    uart.write_str("Letsgo!").ok();
 
     // if you don't comment it out, it works on 08-12 and breaks on 08-13.
     // if you comment this line out on 08-13 everything else starts to work.
-    armv8a_semihosting::hprintln!("Lets {}!", "go").ok(); // culprit
+    uart.write_fmt(format_args_nl!("Lets {}!", "go")).ok();
 
-    armv8a_semihosting::hprintln!("Lets go 2!").ok();
+    uart.write_str("Lets go 2!").ok();
+    uart.flush();
     panic!("Off you go!");
+}
+
+#[panic_handler]
+pub fn handler(_info: &core::panic::PanicInfo) -> ! {
+    crate::endless_sleep()
 }
---
+++ src/write_to.rs
@@ -0,0 +1,105 @@
+/*
+ * SPDX-License-Identifier: BlueOak-1.0.0
+ * Copyright (c) Berkus Decker <berkus+vesper@metta.systems>
+ */
+
+/// No-alloc write!() implementation from https://stackoverflow.com/a/50201632/145434
+/// Requires you to allocate a buffer somewhere manually.
+// @todo Try to use arrayvec::ArrayString here instead?
+use core::{cmp::min, fmt};
+
+pub struct WriteTo<'a> {
+    buffer: &'a mut [u8],
+    // on write error (i.e. not enough space in buffer) this grows beyond
+    // `buffer.len()`.
+    used: usize,
+}
+
+impl<'a> WriteTo<'a> {
+    #[allow(unused)]
+    pub fn new(buffer: &'a mut [u8]) -> Self {
+        WriteTo { buffer, used: 0 }
+    }
+
+    #[allow(unused)]
+    pub fn into_str(self) -> Option<&'a str> {
+        if self.used <= self.buffer.len() {
+            // only successful concats of str - must be a valid str.
+            Some(unsafe { core::str::from_utf8_unchecked(&self.buffer[..self.used]) })
+        } else {
+            None
+        }
+    }
+
+    #[allow(unused)]
+    pub fn into_cstr(self) -> Option<&'a str> {
+        if self.used < self.buffer.len() {
+            self.buffer[self.used] = 0; // Terminate the string
+            Some(unsafe { core::str::from_utf8_unchecked(&self.buffer[..=self.used]) })
+        } else {
+            None
+        }
+    }
+}
+
+impl<'a> fmt::Write for WriteTo<'a> {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        if self.used > self.buffer.len() {
+            return Err(fmt::Error);
+        }
+        let remaining_buf = &mut self.buffer[self.used..];
+        let raw_s = s.as_bytes();
+        let write_num = min(raw_s.len(), remaining_buf.len());
+        remaining_buf[..write_num].copy_from_slice(&raw_s[..write_num]);
+        self.used += raw_s.len();
+        if write_num < raw_s.len() {
+            Err(fmt::Error)
+        } else {
+            Ok(())
+        }
+    }
+}
+
+#[allow(unused)]
+pub fn show<'a>(buffer: &'a mut [u8], args: fmt::Arguments) -> Result<&'a str, fmt::Error> {
+    let mut w = WriteTo::new(buffer);
+    fmt::write(&mut w, args)?;
+    w.into_str().ok_or(fmt::Error)
+}
+
+// Return a zero-terminated str
+#[allow(unused)]
+pub fn c_show<'a>(buffer: &'a mut [u8], args: fmt::Arguments) -> Result<&'a str, fmt::Error> {
+    let mut w = WriteTo::new(buffer);
+    fmt::write(&mut w, args)?;
+    w.into_cstr().ok_or(fmt::Error)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test_case]
+    pub fn write_to_works() {
+        let mut buf = [0u8; 64];
+        let s: &str = show(
+            &mut buf,
+            format_args!("write some stuff {:?}: {}", "foo", 42),
+        )
+        .unwrap();
+        assert_eq!(s, "write some stuff \"foo\": 42");
+        assert_eq!(s.as_ptr(), buf.as_ptr());
+    }
+
+    #[test_case]
+    pub fn zero_terminated_write_to_works() {
+        let mut buf = [0u8; 64];
+        let s: &str = c_show(
+            &mut buf,
+            format_args!("write some stuff {:?}: {}", "foo", 42),
+        )
+        .unwrap();
+        assert_eq!(s, "write some stuff \"foo\": 42\0");
+        assert_eq!(s.as_ptr(), buf.as_ptr());
+    }
+}
