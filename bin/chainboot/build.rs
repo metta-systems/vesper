90e9390cbcf4f4d004c0e8c4284e8b9745667131
Berkus Decker <berkus+github@metta.systems>
2023-08-26 20:05:48 +0300

    fix: 🐛 Fix chainboot linker script
    Part 2: Add linker dependency.

--- bin/chainboot/build.rs
+++ bin/chainboot/build.rs
@@ -1,6 +1,10 @@
+/// This build script is used to link chainboot binary.
+
 const LINKER_SCRIPT: &str = "bin/chainboot/src/link.ld";
+const LINKER_SCRIPT_AUX: &str = "machine/src/arch/aarch64/linker/aarch64-exceptions.ld";
 
 fn main() {
     println!("cargo:rerun-if-changed={}", LINKER_SCRIPT);
+    println!("cargo:rerun-if-changed={}", LINKER_SCRIPT_AUX);
     println!("cargo:rustc-link-arg=--script={}", LINKER_SCRIPT);
 }
