6d2088cb0cf9afcd3eae9db040f276213738f329
Berkus Decker <berkus+github@metta.systems>
2023-08-27 17:23:39 +0300

    chore(logging): 🧹 Remove unused text

--- machine/src/platform/raspberrypi/drivers.rs
+++ machine/src/platform/raspberrypi/drivers.rs
@@ -92,7 +92,7 @@ unsafe fn instantiate_uart() -> Result<(), &'static str> {
 unsafe fn post_init_pl011_uart() -> Result<(), &'static str> {
     #[allow(static_mut_refs)]
     console::register_console(PL011_UART.assume_init_ref());
-    crate::info!("[0] UART0 is live!");
+    crate::info!("UART0 is live!");
     Ok(())
 }
 
