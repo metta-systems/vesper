5ab5dbebc5f7df8f2968b09022935c4712095073
Berkus Decker <berkus+github@metta.systems>
2024-04-20 09:53:00 +0300

    feat: ✨ Extract libcpu

--- libs/cpu/src/cpu/mod.rs
+++ libs/cpu/src/cpu/mod.rs
@@ -16,33 +16,3 @@ pub use arch_cpu::{endless_sleep, nop};
 
 // #[cfg(feature = "test_build")]
 // pub use arch_cpu::{qemu_exit_failure, qemu_exit_success};
-
-/// Loop for a given number of `nop` instructions.
-#[inline]
-pub fn loop_delay(rounds: u32) {
-    for _ in 0..rounds {
-        nop();
-    }
-}
-
-/// Loop until a passed function returns `true`.
-#[inline]
-pub fn loop_until<F: Fn() -> bool>(f: F) {
-    loop {
-        if f() {
-            break;
-        }
-        nop();
-    }
-}
-
-/// Loop while a passed function returns `true`.
-#[inline]
-pub fn loop_while<F: Fn() -> bool>(f: F) {
-    loop {
-        if !f() {
-            break;
-        }
-        nop();
-    }
-}
