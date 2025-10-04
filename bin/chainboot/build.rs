d4e877b66198c9e9ba1b3e4d6c281f88cbcb8610
Berkus Decker <berkus+github@metta.systems>
2025-10-05 00:01:53 +0300

    ❌ Sort dependencies, move properties to workspace root

--- bin/chainboot/build.rs
+++ bin/chainboot/build.rs
@@ -4,7 +4,7 @@ const LINKER_SCRIPT: &str = "bin/chainboot/src/link.ld";
 const LINKER_SCRIPT_AUX: &str = "machine/src/arch/aarch64/linker/aarch64-exceptions.ld";
 
 fn main() {
+    println!("cargo:rerun-if-env-changed=TARGET_BOARD");
     println!("cargo:rerun-if-changed={LINKER_SCRIPT}");
     println!("cargo:rerun-if-changed={LINKER_SCRIPT_AUX}");
-    println!("cargo:rustc-link-arg=--script={LINKER_SCRIPT}");
 }
