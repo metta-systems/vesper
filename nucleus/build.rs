bf1cc7c2b58c7a90ff0d35e70d95f836fd914bb0
Berkus Decker <berkus+github@metta.systems>
2025-10-05 16:04:25 +0300

    refactor(build): 📦 Set up build configuration via cfg instead of features

--- nucleus/build.rs
+++ nucleus/build.rs
@@ -4,7 +4,7 @@ const LINKER_SCRIPT: &str = "machine/src/platform/raspberrypi/linker/kernel.ld";
 const LINKER_SCRIPT_AUX: &str = "machine/src/arch/aarch64/linker/aarch64-exceptions.ld";
 
 fn main() {
+    println!("cargo:rerun-if-env-changed=TARGET_BOARD");
     println!("cargo:rerun-if-changed={LINKER_SCRIPT}");
     println!("cargo:rerun-if-changed={LINKER_SCRIPT_AUX}");
-    println!("cargo:rustc-link-arg=--script={LINKER_SCRIPT}");
 }
